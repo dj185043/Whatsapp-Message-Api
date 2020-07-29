e93b595eed298bbe4f4986012fc4dbe9be3d0116
Dharma Janjanam <Dharma.Janjanam@ncr.com>
2020-07-29 23:18:16 +0530

    Updated with latest changes

--- SendMessage/Models/SendMessage.cs
+++ SendMessage/Models/SendMessage.cs
@@ -12,8 +12,8 @@ namespace SendMessage.Models
     {
         public static string SendWhatsAppMessage(string PhoneNumber, string Message)
         {
-            const string accountSid = "AC085dc98c3c9088e4455917a1e96aa506";
-            const string authToken = "9b3e051ea2d99a8ca2dcf53d401108c8";
+            const string accountSid = string.Empty;
+            const string authToken = string.Empty;
 
             TwilioClient.Init(accountSid, authToken);
 
